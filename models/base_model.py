e03c1c537d87174108e556d4f05dfd137ccc2bb1
“Jonah <etuafuljonah@gmail.com>
2023-11-09 23:19:33 -0800

    updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,3 +3,25 @@
 import models
 from uuid import uuid4
 from datetime import datetime
+
+class BaseModel
+    """Represents the BaseModel of the HBnB project."""
+    def __init__(self, *args, **kwargs)
+        """Initialize a new BaseModel
+
+        Arguments:
+            *args (any): Unused arguments.
+            **kwargs (dict) key-value pairs
+        """
+        t_format = "%Y-%m0%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, t_format)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
